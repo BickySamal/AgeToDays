4091809c29e82961b582dffe013cd0b76c6b4b42
BickySamal <34908641+BickySamal@users.noreply.github.com>
2021-08-21 15:43:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function ageInDays(){
+    var birthyear = prompt("What year were you born?");
+    var year = prompt("What is the current year?")
+    let ans = (year-birthyear)*365;
+    let h1 = document.createElement("h1");
+    let result = document.createTextNode("You are "+ans+" days  old.");
+    // h1.setAttribute("id","ageInDays");
+    h1.appendChild(result);
+    document.getElementById("result").appendChild(h1);
+}
+
+function reset ()
+{
+    document.getElementById("result").remove();
+}
